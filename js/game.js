59ccec347911a01f52bd02bca455518bcdfb3b27
g_rademacher <rademacher.garrett@gmail.com>
2017-11-16 12:05:16 -0500

    fix platform display

--- js/game.js
+++ js/game.js
@@ -1,32 +1,33 @@
+
 PlayState = {};
 
-PlayState.preload = function(){
-  this.game.load.json('level:1', 'platformer/data/level01.json');
-  this.game.load.image('background', 'platformer/images/background.png');
-  this.game.load.image('ground', 'platformer/images/ground.png');
-  this.game.load.image('grass:8x1', 'platformer/images/grass_8x1.png');
-  this.game.load.image('grass:6x1', 'platformer/images/grass_6x1.png');
-  this.game.load.image('grass:4x1', 'platformer/images/grass_4x1.png');
-  this.game.load.image('grass:2x1', 'platformer/images/grass_2x1.png');
-  this.game.load.image('grass:1x1', 'platformer/images/grass_1x1.png');
-}
+PlayState.preload = function () {
+    this.game.load.json('level:1', 'data/level01.json');
 
-PlayState.create = function(){
-  this.game.add.image(0, 0, 'background');
-  this._loadLevel(this.game.cache.getJSON('level:1'));
-}
+    this.game.load.image('background', 'platformer/images/background.png');
+    this.game.load.image('ground', 'platformer/images/ground.png');
+    this.game.load.image('grass:8x1', 'platformer/images/grass_8x1.png');
+    this.game.load.image('grass:6x1', 'platformer/images/grass_6x1.png');
+    this.game.load.image('grass:4x1', 'platformer/images/grass_4x1.png');
+    this.game.load.image('grass:2x1', 'platformer/images/grass_2x1.png');
+    this.game.load.image('grass:1x1', 'platformer/images/grass_1x1.png');
+};
 
-PlayState._loadLevel = function(data){
-  //spawn the platforms
-  data.platforms.forEach(this._spawnPlatform, this);
-}
+PlayState.create = function () {
+    this.game.add.image(0, 0, 'background');
+    this._loadLevel(this.game.cache.getJSON('level:1'));
+};
 
-PlayState._spawnPlatform = function(platform) {
-  this.game.add.sprite(platform.x, platform.y, platform.image);
-}
+PlayState._loadLevel = function (data) {
+    // spawn all platforms
+    data.platforms.forEach(this._spawnPlatform, this);
+};
 
+PlayState._spawnPlatform = function (platform) {
+    this.game.add.sprite(platform.x, platform.y, platform.image);
+};
 window.onload = function (){
   let game =  new Phaser.Game(960, 600, Phaser.AUTO, 'game');
   game.state.add('play', PlayState);
   game.state.start('play');
-}
+};
