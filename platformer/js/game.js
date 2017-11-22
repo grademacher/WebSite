b72c86cd40a08e8ff857f6da9ca6efd65c5ddf56
g_rademacher <rademacher.garrett@gmail.com>
2017-11-21 18:51:05 -0600

    changed file locations

--- platformer/js/game.js
+++ platformer/js/game.js
@@ -109,27 +109,27 @@ PlayState.init = function () {
 };
 
 PlayState.preload = function () {
-    this.game.load.json('level:1', 'data/level01.json');
+    this.game.load.json('level:1', 'platformer/data/level01.json');
 
-    this.game.load.image('font:numbers', 'images/numbers.png');
+    this.game.load.image('font:numbers', 'platformer/images/numbers.png');
 
-    this.game.load.image('background', 'images/background.png');
-    this.game.load.image('ground', 'images/ground.png');
-    this.game.load.image('grass:8x1', 'images/grass_8x1.png');
-    this.game.load.image('grass:6x1', 'images/grass_6x1.png');
-    this.game.load.image('grass:4x1', 'images/grass_4x1.png');
-    this.game.load.image('grass:2x1', 'images/grass_2x1.png');
-    this.game.load.image('grass:1x1', 'images/grass_1x1.png');
-    this.game.load.image('hero', 'images/hero_stopped.png');
-    this.game.load.image('invisible-wall', 'images/invisible_wall.png');
-    this.game.load.image('icon:coin', 'images/coin_icon.png');
+    this.game.load.image('background', 'platformer/images/background.png');
+    this.game.load.image('ground', 'platformer/images/ground.png');
+    this.game.load.image('grass:8x1', 'platformer/images/grass_8x1.png');
+    this.game.load.image('grass:6x1', 'platformer/images/grass_6x1.png');
+    this.game.load.image('grass:4x1', 'platformer/images/grass_4x1.png');
+    this.game.load.image('grass:2x1', 'platformer/images/grass_2x1.png');
+    this.game.load.image('grass:1x1', 'platformer/images/grass_1x1.png');
+    this.game.load.image('hero', 'platformer/images/hero_stopped.png');
+    this.game.load.image('invisible-wall', 'platformer/images/invisible_wall.png');
+    this.game.load.image('icon:coin', 'platformer/images/coin_icon.png');
 
-    this.game.load.spritesheet('coin', 'images/coin_animated.png', 22, 22);
-    this.game.load.spritesheet('spider', 'images/spider.png', 42, 32);
+    this.game.load.spritesheet('coin', 'platformer/images/coin_animated.png', 22, 22);
+    this.game.load.spritesheet('spider', 'platformer/images/spider.png', 42, 32);
 
-    this.game.load.audio('sfx:jump', 'audio/jump.wav');
-    this.game.load.audio('sfx:coin', 'audio/coin.wav');
-    this.game.load.audio('sfx:stomp', 'audio/stomp.wav');
+    this.game.load.audio('sfx:jump', 'platformer/audio/jump.wav');
+    this.game.load.audio('sfx:coin', 'platformer/audio/coin.wav');
+    this.game.load.audio('sfx:stomp', 'platformer/audio/stomp.wav');
 };
 
 PlayState.create = function () {
